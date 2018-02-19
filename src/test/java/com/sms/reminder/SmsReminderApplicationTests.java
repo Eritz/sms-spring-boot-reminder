ce012b9b1ce872d7478d2e373dd3dadb49d8a72e
Eddie <eddieleon93@gmail.com>
2018-02-19 10:37:20 -0500

    Added maven-surefire plugin & apache for local

--- src/test/java/com/sms/reminder/SmsReminderApplicationTests.java
+++ src/test/java/com/sms/reminder/SmsReminderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.twilio.sms.reminder;
+package com.sms.reminder;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SmsTwilioReminderApplicationTests {
+public class SmsReminderApplicationTests {
 
 	@Test
 	public void contextLoads() {
